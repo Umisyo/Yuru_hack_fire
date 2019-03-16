6f1dc31bedeb6b71ee11ec01ba3e2084595abf6a
Umisyo <kusunokisouta@gmail.com>
2019-03-16 21:34:45 +0900

    test

---
+++ test.js
@@ -0,0 +1,5 @@
+var test = 'Hello! firebase!';
+function Test(msg) {
+    console.log(msg);
+}
+Test(test);
---
+++ test.ts
@@ -0,0 +1,7 @@
+const test: string = 'Hello! firebase!'
+
+function Test(msg: string){
+    console.log(msg)
+}
+
+Test(test)
